1bc88e031abe4f8c432523743a8930f1cf507394
muhammad adib bin ahmad roslan <adbroslan@gmail.com>
2019-08-31 14:07:36 +0800

    lesson done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'mywidget.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,22 +9,73 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Material App',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('First App'),
-        ),
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Column(
-              children: <Widget>[Text('column1'), Text('column1')],
-            ),
-            Column(
-              children: <Widget>[Text('column2'), Text('column2')],
-            ),
-            MyWidget()
-          ],
-        ),
-      ),
+          appBar: AppBar(
+            title: Text('First App'),
+          ),
+          body: Home()),
     );
   }
 }
+
+// Button and alert
+// class Home extends StatelessWidget {
+//   const Home({Key key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//         child: IconButton(
+//       icon: Icon(Icons.accessible_forward),
+//       iconSize: 90,
+//       tooltip: 'Increase volume by 10',
+//       onPressed: () {
+//         showDialog(
+//           context: context,
+//           barrierDismissible: false, // user must tap button!
+//           builder: (BuildContext context) {
+//             return CupertinoAlertDialog(
+//               title: Text('Rewind and remember'),
+//               content: SingleChildScrollView(
+//                 child: ListBody(
+//                   children: <Widget>[
+//                     Text('You will never be satisfied.'),
+//                     Text('You\’re like me. I’m never satisfied.'),
+//                   ],
+//                 ),
+//               ),
+//               actions: <Widget>[
+//                 FlatButton(
+//                   child: Text('Regret'),
+//                   onPressed: () {
+//                     Navigator.of(context).pop();
+//                   },
+//                 ),
+//               ],
+//             );
+//           },
+//         );
+//       },
+//     ));
+//   }
+// }
+
+// //snackbar
+// class Home extends StatelessWidget {
+//   const Home({Key key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//         child: IconButton(
+//       icon: Icon(Icons.accessible_forward),
+//       iconSize: 90,
+//       tooltip: 'Increase volume by 10',
+//       onPressed: () {
+//         final snackbar = SnackBar(
+//           content: Text('test'),
+//         );
+//         Scaffold.of(context).showSnackBar(snackbar);
+//       },
+//     ));
+//   }
+// }
